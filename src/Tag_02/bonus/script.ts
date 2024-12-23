df3f6c1be6fa3907dced290a0f1583a62abe16fe
Magda <supercoder@192.168.100.5>
2024-12-23 10:34:06 +0100

    articleText = article.innerText zeile 17 TS

--- src/Tag_02/bonus/script.ts
+++ src/Tag_02/bonus/script.ts
@@ -12,27 +12,29 @@ const clicktoSearch = () => {
     // const searchInput = (document.getElementById("search-input") as HTMLInputElement).value
     // ? value direkt bei document abfragen -> vorteil: verkürzte schriebweise und value muss nur einmal abgefragt werden
 
-    const article = document.querySelector("article");
+    let article = document.querySelector("article") as HTMLElement;
     const hinweisDiv = document.querySelector("div");
+    const articleText = article.innerText; // ? innerText ignoriert HTML-Tags und such nur den reienten Text
+    const searchValue = searchInput.value.trim();
 
     // ! Prüfen, ob etwas angegeben wurde
     // trim() ->  Entfernt die Leerzeichen am Anfang und Ende der String so, wie er eingegeben wurde
-    if (!searchInput.value.trim()) {
+    if (!searchValue) {
         return;
     }
 
     // ! Gesuchtes Wort wird gelb und fett hervorgehoben
     // const highlight = `<span style="background-color: yellow; font-weight: bold; color: black">${searchInput.value}</span>`;
-    const highlight = `<span class="highlight">${searchInput.value}</span>`;
+    const highlight = `<span class="highlight">${searchValue}</span>`;
 
 
     // ! Prüfen, ob das Wort im Artikel vorhanden ist
      // ? wird nur ausgeführt, wenn der erste Teil (article) true ist.
     
     //  Wenn das wort im Artikel ist: !true wird false, und der if-Block wird nicht ausgeführt.
-    if (article && !article.innerHTML.includes(searchInput.value)) {
+    if (article && !articleText.includes(searchValue)) {
         if (hinweisDiv) {
-            hinweisDiv.innerHTML = `<p class="alert">Das Wort "${searchInput.value}" ist nicht im Text enthalten</p>`;
+            hinweisDiv.innerHTML = `<p class="alert">Das Wort "${searchValue}" ist nicht im Text enthalten</p>`;
         // Wenn das Wort im Artikel nicht vorhanden ist: !false wird true, und der if-Block wird ausgeführt.
         }
         return;
@@ -40,12 +42,12 @@ const clicktoSearch = () => {
     
     // Fehlermeldung verschwindet, wenn das richtige Wort angegeben wurde
     if (hinweisDiv) {
-        hinweisDiv.innerHTML = "";
+        hinweisDiv.innerText = "";
     }
 
     // ! Ersetzen, wenn Artikel existiert
     if (article) {
-        article.innerHTML = article.innerHTML.replaceAll(searchInput.value, highlight);
+        article.innerHTML = article.innerHTML.replaceAll(searchValue, highlight);
     }
 };
 
