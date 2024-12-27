ef0271a0308fceb2670afbb148f8d9dc46fd5c10
Magda <supercoder@192.168.100.5>
2024-12-27 13:28:43 +0100

    aufgabe 3

--- src/Tag_04/Aufgabe_03/script.ts
+++ src/Tag_04/Aufgabe_03/script.ts
@@ -1,6 +1,57 @@
-// - In dieser Übung werden wir [Date](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date?retiredLocale=de)() lernen.
-// - Stelle die folgenden Daten im HTML-Dokument mit Hilfe des Befehls new Date() dar.
-// - Speichere die Werte in Variablen (date1, date2, usw.).
-// - Verwende innerHTML, um das Ergebnis darzustellen.
-// - Teste die folgenden Werte:  new Date("September 2, 2019 09:00:00")  new Date(0)  new Date(31556908800)  new Date(86400000)
-// - **Bitte halte dich an den Typescript-Standard.**
+// - ernziel: Verwendung der Library [date-fns](https://date-fns.org/docs/Getting-Started)
+// - Installiere dir mit npm i date-fns die Bibliothek in deinem Projekt
+// - Lege zwei Datumsvariablen an: 01.01.1970 und jetzt
+// - Nutze die Funktionen differenceInYears, …Hours, …Seconds
+    
+//     → Lass dir jeweils den Unterschied für die beiden Datumswerte auf der Konsole ausgeben
+    
+// - Lege eine weitere Variable myBirthday an Berechne mit date-fns dein aktuelles Alter und dein Alter am 07.04.2007
+// - Nutze Format um Informationen zu deinem Geburtstag in folgender Weise auszugeben
+
+// "20.12.1979 12:40:00"
+// "20.12.1979 12:40"
+// "20.12.79"
+// "20. Dezember 1979"
+// "12:40"
+// "Donnerstag"
+// "Dezember"
+// "20 des Dezember im Jahre des Herrn 1979"
+
+import { format, differenceInYears, differenceInHours, differenceInSeconds } from 'date-fns';
+import { de } from 'date-fns/locale';
+// - https://date-fns.org/v4.1.0/docs/I18n
+
+// - Lege zwei Datumsvariablen an: 01.01.1970 und jetzt
+const date1 = new Date(1970, 0, 1); 
+const now = new Date(); 
+
+// - Nutze die Funktionen differenceInYears, …Hours, …Seconds
+const differenz1 = differenceInYears(now, date1);
+const differenz2 = differenceInHours(now, date1);
+const differenz3 = differenceInSeconds(now, date1);
+
+console.log(`Differenz in Jahren: ${differenz1}`);
+console.log(`Differenz in Stunden: ${differenz2}`);
+console.log(`Differenz in Sekunden: ${differenz3}`);
+
+// - Geburtstag und Alter an einem bestimmten Tag
+const meinGeb = new Date(1989, 11, 30, 11, 40);
+const gebAmTag = new Date(2007, 3, 7);
+
+// - Lege eine weitere Variable myBirthday an Berechne mit date-fns dein aktuelles Alter und dein Alter am 07.04.2007
+const alterJetzt = differenceInYears(now, meinGeb);
+const alterAmTag = differenceInYears(gebAmTag, meinGeb);
+
+console.log(`Aktuelles Alter: ${alterJetzt}`);
+console.log(`Alter am 07.04.2007: ${alterAmTag}`);
+
+
+console.log(format(meinGeb, 'dd.MM.yyyy HH:mm:ss'));
+console.log(format(meinGeb, 'dd.MM.yyyy HH:mm'));
+console.log(format(meinGeb, 'dd.MM.yy'));
+console.log(format(meinGeb, 'dd. MMMM yyyy'));
+console.log(format(meinGeb, 'HH:mm' , {locale: de}));
+console.log(format(meinGeb, 'EEEE' , {locale: de}))
+console.log(format(meinGeb, 'MMMM' , {locale: de}));
+console.log(format(meinGeb, "dd 'des' MMMM 'im Jahre des Wurms' yyyy"));
+
